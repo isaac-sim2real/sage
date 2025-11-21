2991aef1311d051f65f702faeb0ce1646ad0139e
Dikai Liu <dikail@nvidia.com>
2025-11-22 07:43:41 +0800

    refactor to be based on isaaclab
    Signed-off-by: Dikai Liu <dikail@nvidia.com>

---
+++ sage/robots/realman.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
+"""Configuration for RealMan robots."""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import ArticulationCfg
+
+##
+# Configuration
+##
+
+WR75S_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path="assets/realman_WR75S/realman_WR75S.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=8, solver_velocity_iteration_count=4
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.01),
+        joint_pos={".*": 0.0},
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "arms": ImplicitActuatorCfg(
+            joint_names_expr=["[l,r]_joint[1-7]"],
+            effort_limit_sim=300,
+            stiffness=1000.0,
+            damping=200.0,
+        ),
+    },
+)
+"""Configuration for RealMan WR75S humanoid robot."""
